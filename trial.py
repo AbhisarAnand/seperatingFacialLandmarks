43a554c4d25327710a9d735df3f1cc72e595b0cb
abhisar <abhisar.muz@gmail.com>
2020-03-23 14:16:27 -0400

    Just shows the two eyes and the mouth. Everything else is black. This can help in removing background noise.

--- trial.py
+++ trial.py
@@ -2,28 +2,20 @@
 # python detect_blinks.py --shape-predictor shape_predictor_68_face_landmarks.dat --video blink_detection_demo.mp4
 # python detect_blinks.py --shape-predictor shape_predictor_68_face_landmarks.dat
 
+import cv2
+import dlib
+import imutils
 # import the necessary packages
 import numpy
-from imutils.video import VideoStream
-from imutils import face_utils
-import numpy as np
-import argparse
-import imutils
 import time
-import dlib
-import cv2
+from imutils import face_utils
 
-# construct the argument parse and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-p", "--shape-predictor", required=True,
-                help="path to facial landmark predictor")
-args = vars(ap.parse_args())
-
+path = '/home/abhisar/PycharmProjects/seperatingFacialLandmarks/utils/shape_predictor_68_face_landmarks (1).dat'
 # initialize dlib's face detector (HOG-based) and then create
 # the facial landmark predictor
 print("[INFO] loading facial landmark predictor...")
 detector = dlib.get_frontal_face_detector()
-predictor = dlib.shape_predictor(args["shape_predictor"])
+predictor = dlib.shape_predictor(path)
 
 # grab the indexes of the facial landmarks for the left and
 # right eye, respectively
@@ -31,61 +23,47 @@ predictor = dlib.shape_predictor(args["shape_predictor"])
 (rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
 (mStart, mEnd) = face_utils.FACIAL_LANDMARKS_IDXS["mouth"]
 
-vs = VideoStream(src=0).start()
+vs = cv2.VideoCapture("/dev/video0")
 fileStream = False
 time.sleep(1.0)
 
 # loop over frames from the video stream
 while True:
-    # if this is a file video stream, then we need to check if
-    # there any more frames left in the buffer to process
-    if fileStream and not vs.more():
-        break
 
     # grab the frame from the threaded video file stream, resize
     # it, and convert it to grayscale
     # channels)
-    frame = vs.read()
-    fframe = frame.copy()
+    _, frame = vs.read()
     frame = imutils.resize(frame, width=450)
+    fframe = frame.copy()
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    # detect faces in the grayscale frame
     rects = detector(gray, 0)
-
-    # loop over the face detections
     for rect in rects:
-        # determine the facial landmarks for the face region, then
-        # convert the facial landmark (x, y)-coordinates to a NumPy
-        # array
         shape = predictor(gray, rect)
         shape = face_utils.shape_to_np(shape)
-
-        # extract the left and right eye coordinates, then use the
-        # coordinates to compute the eye aspect ratio for both eyes
         leftEye = shape[lStart:lEnd]
         rightEye = shape[rStart:rEnd]
         mouth = shape[mStart:mEnd]
 
-        # compute the convex hull for the left and right eye, then
-        # visualize each of the eyes
-        leftEyeHull = cv2.convexHull(leftEye)
-        rightEyeHull = cv2.convexHull(rightEye)
-        mouthHull = cv2.convexHull(mouth)
-        a = cv2.drawContours(frame, [leftEyeHull], -1, (255, 255, 255), 1)
-        b = cv2.drawContours(frame, [rightEyeHull], -1, (255, 255, 255), 1)
-        c = cv2.drawContours(frame, [mouthHull], -1, (255, 255, 255), 1)
+        leftEyeHull = cv2.boundingRect(leftEye)
+        x, y, w, h = leftEyeHull
+        rightEyeHull = cv2.boundingRect(rightEye)
+        x1, y1, w1, h1 = rightEyeHull
+        mouthHull = cv2.boundingRect(mouth)
+        x2, y2, w2, h2 = mouthHull
+        a = cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 255, 255), -1)
+        b = cv2.rectangle(frame, (x1, y1), (x1 + w1, y1 + h1), (255, 255, 255), -1)
+        c = cv2.rectangle(frame, (x2, y2), (x2 + w2, y2 + h2), (255, 255, 255), -1)
         white_color = numpy.array([255, 255, 255])
         white_mask = cv2.inRange(frame, white_color, white_color)
         white = cv2.bitwise_and(frame, frame, mask=white_mask)
         finalMask = cv2.copyTo(fframe, white)
-
-
+        frame = finalMask
 
     # show the frame
-    cv2.imshow("Frame", finalMask)
+    cv2.imshow("Frame", frame)
     key = cv2.waitKey(1) & 0xFF
-
     # if the `q` key was pressed, break from the loop
     if key == ord("q"):
         break
